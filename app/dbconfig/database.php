76fa9e1be3e90e6d6b9fb02d02d591a9b7f06864
Tech Support <contactus@stratosysit.com>
2017-01-07 12:52:14 -0800

    new db php

--- app/dbconfig/database.php
+++ app/dbconfig/database.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("198.199.86.251", "forge", "TQQBa92Hb0hXZHcWxidz", "forge");
+$mysqli = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD'], $_ENV['DB_NAME']);
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
