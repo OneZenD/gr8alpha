63eb25852eec79e6d37523e228b76d51997489d3
Tech Support <contactus@stratosysit.com>
2017-01-07 13:09:48 -0800

    test

--- app/dbconfig/database.php
+++ app/dbconfig/database.php
@@ -1,19 +1,5 @@
 <?php
 
-'mysql' => [
-    'driver'    => 'mysql',
-    'host'      => env('DB_HOST', 'localhost'),
-    'database'  => env('DB_DATABASE', 'forge'),
-    'username'  => env('DB_USERNAME', 'forge'),
-    'password'  => env('DB_PASSWORD', ''),
-    'charset'   => 'utf8',
-    'collation' => 'utf8_unicode_ci',
-    'prefix'    => '',
-    'strict'    => false,
-],
-
-
-
 $mysqli = new mysqli(_ENV('DB_HOST'), _ENV['DB_USERNAME'], $_ENV['DB_PASSWORD'], $_ENV['DB_NAME']);
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
