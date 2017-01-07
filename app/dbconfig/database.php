1ca9dbd1070689aabb818033985f1cd4ef38bb88
Tech Support <contactus@stratosysit.com>
2017-01-07 14:03:45 -0800

    test

--- app/dbconfig/database.php
+++ app/dbconfig/database.php
@@ -8,7 +8,7 @@ $password = "TQQBa92Hb0hXZHcWxidz";
 /* echo "<p> Monkey </p>";
 echo $database; */
 
-$mysqli = new mysqli("example.com", "user", "password", "database");
+$mysqli = new mysqli($host, $username, $password, $database);
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
